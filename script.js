5eaa24fc8048131cc5e2fdb789fa35e50a33f8f1
Prabhjot Kaur <prabhjotkaur@Prabhjots-MacBook-Air.local>
2026-04-05 22:53:23 +0530

    added leaderboard feature

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 fetch("https://api.github.com/repos/prabhjotkaurarora27/gitboard/commits")
   .then(response => response.json())
   .then(data => {
-    const list = document.getElementById("commits");
+    const commitList = document.getElementById("commits");
+    const leaderboard = document.getElementById("leaderboard");
+
+    let contributors = {};
 
     data.forEach(commit => {
+      // Show commits
       const li = document.createElement("li");
       li.textContent = commit.commit.message;
-      list.appendChild(li);
+      commitList.appendChild(li);
+
+      // Count commits per person
+      const author = commit.commit.author.name;
+
+      if (contributors[author]) {
+        contributors[author]++;
+      } else {
+        contributors[author] = 1;
+      }
     });
+
+    // Create leaderboard
+    for (let person in contributors) {
+      const li = document.createElement("li");
+      li.textContent = person + " - " + contributors[person] + " commits";
+      leaderboard.appendChild(li);
+    }
   });
 
 
